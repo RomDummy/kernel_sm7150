388d2fd301bfa43efb1c52fc7dad861ab205043f
RomDummy <rooghz720@gmail.com>
2023-06-28 22:37:36 +0800

    aw8624_haptic: rename to -> oss and working with magic installer
    diff driver 1.0.9
    
    Signed-off-by: RomDummy <rooghz720@gmail.com>

--- drivers/input/misc/aw8624_haptic_oss/aw8624.c
+++ drivers/input/misc/aw8624_haptic_oss/aw8624.c
@@ -31,6 +31,7 @@
 #include <linux/syscalls.h>
 #include <linux/power_supply.h>
 #include <linux/pm_qos.h>
+#include <misc/aghisna_haptic.h>
 #include "aw8624_reg.h"
 #include "aw8624.h"
 
@@ -4290,7 +4291,10 @@ static int __init aw8624_i2c_init(void)
 
 	pr_info("aw8624 driver version %s\n", AW8624_VERSION);
 
-	ret = i2c_add_driver(&aw8624_i2c_driver);
+	if (haptic_qti_oss) {
+		ret = i2c_add_driver(&aw8624_i2c_driver);
+	}
+	
 	if (ret) {
 		pr_err("fail to add aw8624 device into i2c\n");
 		return ret;
